ad6fe543ac2d974a68e2bf70a9b12ee957cd634d
saad ahmed <saadahmed1742@gmail.com>
2022-12-16 21:07:12 +0500

    completed metaverse component

--- src/components/home/tokenization.jsx
+++ src/components/home/tokenization.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import vector from "../../assets/morachain-vector.png"
 const Tokenization = () => {
   return (
-    <section class=" body-font">
-  <div class="container px-8 md:px-[20px] lg:px-5 py-24 mx-auto flex flex-wrap">
+    <section className=" body-font">
+  <div className="container px-8 md:px-[20px] lg:px-5 py-24 mx-auto flex flex-wrap">
     <div className='md:w-2/4 flex items-end '>
         <img src={vector} alt="" />
     </div>
-    <div class="md:w-2/4 ">
-      <div class="md:mt-4 mt-6 lg:pl-[20px] bg">
+    <div className="md:w-2/4 ">
+      <div className="md:mt-4 mt-6 lg:pl-[20px] bg">
         <h1 className='text-3xl lg:text-[40px] font-bold'><span className='text-[#e1b646]'>Tokenization</span> Platform</h1>
         <br />
         <p className='text-gray-400 text-[18px] md:text-[16px] lg:text-[20px] md:font-medium'>Morachain has a platform that tokenizes property resources in the real worth of real estate metrics that is Sq. ft. A minimum share in the property is 1 sq. ft. that can be bought. With properties going from reasonable housing to ultra-luxury - the tokenization platform, Morachain, was launched in the year XYZ exclusively.</p>
--- src/components/home/whymorchain.jsx
+++ src/components/home/whymorchain.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-
+import accesibility from "../../assets/accessiblity.png"
+import accesibilityvector from "../../assets/accesibiltyvector.png"
+import freedom from "../../assets/freedom.png"
+import freedomvector from "../../assets/freedomvector.png"
+import creativity from "../../assets/creativity.png"
+import creativityvector from "../../assets/creativityvector.png"
+import diversity from "../../assets/diversity.png"
+import diversityvector from "../../assets/diversityvector.png"
 const Whymorchain = () => {
   return (
     <section className="text-gray-600 body-font">
@@ -8,9 +15,54 @@ const Whymorchain = () => {
       <h1 className="sm:text-5xl text-2xl font-bold title-font mb-4 text-gray-900">Why <span className='text-[#e1b646] font-bold'>Morachain</span></h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed font-medium text-[18px]">Morachain offers a unique way for opening and unlocking NFT liquidity and gives local community admittance to possessing portions of notable and valuable Real Estate NFTs.</p>
     </div>
-    <div className="flex flex-wrap -m-4">
-      <div>
-
+    <div className="flex flex-wrap justify-center p-[20px] -m-3">
+      <div className='relative sm:w-[250px]  flex flex-col items-center rounded-[20px] shadow-black shadow-sm mb-[20px] sm:mr-[20px] p-[5px]'>
+      <div className='absolute bottom-0 right-0'>
+            <img src={accesibilityvector} alt="" />
+      </div>
+      <div className='my-[20px]'>
+        <img src={accesibility} alt="" />
+      </div>
+      <div className='mb-[40px]'>
+        <h1 className='text-center mb-[10px] font-extrabold text-[25px]'>ACCESSIBILITY</h1>
+        <p className='text-center font-semibold md:font-semibold  md:text-[15px] text-[#aba8a0]'>Get admittance to the best real estate deals that were simply accessible to institutional investors. Invest in land in fractions and gain profits with capital appreciation.</p>
+      </div> 
+      </div>
+      <div className='relative sm:w-[250px]  flex flex-col items-center rounded-[20px] shadow-black shadow-sm mb-[20px] sm:mr-[20px] p-[5px]'>
+      <div className='absolute bottom-0 right-0'>
+            <img src={freedomvector} alt="" />
+      </div>
+      <div className='my-[20px]'>
+        <img src={freedom} alt="" />
+      </div>
+      <div className='mb-[40px]'>
+        <h1 className='text-center mb-[10px] font-extrabold text-[25px]'>FREEDOM</h1>
+        <p className='text-center font-semibold md:font-semibold  md:text-[15px] text-[#aba8a0]'>No lock-in period on investments and no barrier to selective deals delivered on the tokenization stage.</p>
+      </div> 
+      </div>
+      <div className='relative sm:w-[250px]   flex flex-col items-center rounded-[20px] shadow-black shadow-sm mb-[20px] sm:mr-[20px] p-[5px]'>
+      <div className='absolute bottom-0 right-0'>
+            <img src={diversityvector} alt="" />
+      </div>
+      <div className='my-[20px]'>
+        <img src={diversity} alt="" />
+      </div>
+      <div className='mb-[40px]'>
+        <h1 className='text-center mb-[10px] font-extrabold text-[25px]'>DIVERSITY</h1>
+        <p className='text-center font-semibold md:font-semibold  md:text-[15px] text-[#aba8a0]'>Get deals of investments from affordable housing to extravagance and ultra-private properties in fractions. Admittance to worldwide properties and other resource classes like business, retail, and hospitality.</p>
+      </div> 
+      </div>
+      <div className='relative z-[99] sm:w-[250px]  flex flex-col items-center rounded-[20px] shadow-black shadow-sm mb-[20px] sm:mr-[20px] p-[5px]'>
+      <div className='absolute bottom-1 right-2'>
+            <img src={creativityvector} alt=""/>
+      </div>
+      <div className='my-[20px]'>
+        <img src={creativity} alt="" />
+      </div>
+      <div className='mb-[40px] z-[999]'>
+        <h1 className='text-center mb-[10px] font-extrabold text-[25px]'>CREATIVITY</h1>
+        <p className='text-center font-semibold md:font-semibold md:text-[15px] text-[#aba8a0]'>With the P2E opportunity accessible the users have unlimited possibilities in the mind of what can be created and adapted.</p>
+      </div> 
       </div>
     </div>
   </div>
