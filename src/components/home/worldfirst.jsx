9e45741283a8f261a9ffdf448b3611d3125247ae
saad ahmed <saadahmed1742@gmail.com>
2022-12-22 15:23:14 +0500

    completed responsive

--- src/components/home/worldfirst.jsx
+++ src/components/home/worldfirst.jsx
@@ -10,13 +10,13 @@ const Worldfirst = () => {
       backgroundSize: "cover"
     }} className="lady relative mt-[200px] lg:mt-[0px] flex justify-center pb-[50px] h-[1050px] sm:h-[1000px] md:h-[1100px] lg:h-[620px]">
       <div className="bgs z-0"></div>
-      <div className="flex z-50 items-center flex-col-reverse lg:flex-row  justify-between w-[90%] md:w-[70%]">
-      <div className=" flex flex-col justify-center text-white xl:w-[60%] ">
+      <div className="flex z-50 items-center flex-col-reverse lg:flex-row  justify-between w-[90%] md:w-[70%] ">
+      <div className=" flex flex-col justify-center text-white xl:w-[100%]">
         <h1 className="text-[25px] md:text-[40px] font-bold">World’s First Decentralized Real Estate NFT Marketplace with Metaverse</h1>
         <p className="md:text-[18px] pt-[10px]">Morachain is the trailblazer in Fractionalizing the NFT of real estate assets. We have likewise changed this by making a computerized resource of this real-world property asset. We at Morachain have zeroed in on giving a consistent experience with transactions in a digital format by canceling out all middlemen through this platform.</p>
         <p className="text-[15px] md:text-[18px] pt-[10px]">Morachain is the trailblazer in Fractionalizing the NFT of real estate assets. We have likewise changed this by making a computerized resource of this real-world property asset. We at Morachain have zeroed in on giving a consistent experience with transactions in a  through this platform.</p>
       </div>
-      <div className="w-full flex justify-center md:justify-end ">
+      <div className="w-full lg:w-[50%] flex justify-center md:justify-end ">
         <img src={lady} alt="" className="md:h-[700px]" />
       </div>
       </div>
